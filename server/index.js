5fe995816a84b52e72dce03163ff14c9bce9e407
KALIL DOMINGOS PORTO <kalil_porto@estudante.sesisenai.org.br>
2024-10-21 09:50:20 -0300

    um

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 import  Express  from 'express'
 
 const app = Express()
+app.use(Express.json())
 
 app.get('/pegar', function(req, res) {
     res.send('enviar esta mensagem')
 })
 app.get('/pegaroutracoisa', function(req, res) {
     res.send('esta é outra mensagem')
+})
+
+app.post('/registro', function(req,res){
+    try{
+        console.log(req.body)
+        const {nome, sobrenome, email, senha, dataNascimento} = req.body
+        if(!nome || !sobrenome || email ||senha ||dataNascimento){
+        res.status(406).send('todos os campos devem ser enviados')
+        return
+
+        }console.log('criar user')
+        res.status(201).send('ok')
+    } catch (erro) {
+        
+    }
 })
 
 app.listen(8000)
