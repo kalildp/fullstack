0162e8bc2715cc1793208a5a7c2d805b9c82bad5
KALIL DOMINGOS PORTO <kalil_porto@estudante.sesisenai.org.br>
2024-10-24 12:01:43 -0300

    add api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 import  Express  from 'express'
+import {User, criarTabelas} from './db.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const app = Express()
 app.use(Express.json())
@@ -9,20 +12,68 @@ app.get('/pegar', function(req, res) {
 app.get('/pegaroutracoisa', function(req, res) {
     res.send('esta é outra mensagem')
 })
-
-app.post('/registro', function(req,res){
+//criarTabelas()
+app.post('/registro', async function(req,res){
     try{
         console.log(req.body)
         const {nome, sobrenome, email, senha, dataNascimento} = req.body
-        if(!nome || !sobrenome || email ||senha ||dataNascimento){
-        res.status(406).send('todos os campos devem ser enviados')
-        return
 
-        }console.log('criar user')
+        if(!nome || !sobrenome || !email || !senha || !dataNascimento){
+            res.status(406).send('todos os campos devem ser enviados')
+            return
+        }
+        if(await User.findOne({where:{email:email}})){
+            res.status(400).send('usuario ja existe')
+            return
+        }
+        const senhaSegura = bcryptjs.hashSync(senha, 10)
+        const novoUsuario = User.create({
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email,
+            senha: senhaSegura,
+            dataNascimento: dataNascimento
+        })
         res.status(201).send('ok')
     } catch (erro) {
         
     }
+})
+
+app.post('/login', async function(req,res) {
+    try{
+       const { email, senha} = req.body
+    if (!email || !senha) {
+        res.status(400).send("todos os campos devem ser preencidos")
+        return
+    } 
+    const usuario = await User.findOne({where:{email:email}})
+    if (!usuario) {
+        res.send('este email nao esta cadastrado')
+        return
+    }
+    const senhaCorreta = bcryptjs.compareSync(senha, usuario.senha)
+    if (!senhaCorreta) {
+        res.send('este senha nao esta correta')
+        return
+    }
+    const token =jwt.sign(
+        {
+            nome:usuario.nome,
+            email:usuario.email,
+            status:usuario.status
+        },
+        'chavecriptografiasupersegura',
+        {expiresIn: 1000*60+60*24*30}
+    )
+    res.send({msg:'voce foi logado', token: token})
+
+}
+    catch (erro){
+        console.log(erro)
+        res.status(500).send("houve um problema")
+    }
+    
 })
 
 app.listen(8000)
